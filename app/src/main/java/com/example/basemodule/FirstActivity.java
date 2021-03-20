a2fb4fc9e8d7de6fe31bfa4c7f1b6b610a721eda
451083622 <451083622@qq.com>
2021-03-20 14:24:05 +0800

    取消基类入口

--- app/src/main/java/com/example/basemodule/FirstActivity.java
+++ app/src/main/java/com/example/basemodule/FirstActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.blankj.utilcode.util.SPUtils;
+
 public class FirstActivity extends AppCompatActivity {
 
     @Override
